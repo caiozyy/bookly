b8e28228a795b73c17fa3e36d3d470b0fd6b1000
Caio Corrêa <searchoncaiozinho@gmail.com>
2025-03-31 17:10:32 -0300

    construindo o auth de registro no aplicativo

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -11,13 +12,28 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    password:{
+    password: {
         type: String,
         required: true,
         minlength: 6
     },
-    profileImage: { 
+    profileImage: {
         type: String,
         default: ""
     }
 });
+
+// hash password before saving user to db 
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next()
+})
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,67 @@
 import express from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
+const generateToken = (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+}
+
 router.post("/register", async (req, res) => {
-    res.send("register");
+    try {
+        const { email, username, password } = req.body;
+
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "Todos os campos são obrigátorios" });
+        }
+
+        if (password.lengh < 6) {
+            return res.status(400).json({ message: "Password should be at least 6 characters long" });
+        }
+
+        if (username.lengh < 3) {
+            return res.status(400).json({ message: "Username should be at least 3 characters long" });
+        }
+
+        // verificar se o usuario já existe 
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({ message: "Esse email já exite" });
+        }
+        const existingUsername = await User.findOne({ username });
+        if (existingUsername) {
+            return res.status(400).json({ message: "Esse usuario já exite" });
+        }
+
+        // pega uma imagem random
+        const profileImage = `http://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
+        const user = new User({
+            email,
+            username,
+            password,
+            profileImage,
+        })
+
+        await user.save();
+
+        const token = generateToken(user._id);
+
+        res.status(201).json({
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                profileImage: user.profileImage
+            },
+        });
+    } catch (error) {
+        console.log("Error in register route", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 });
 router.post("/login", async (req, res) => {
     res.send("login");
